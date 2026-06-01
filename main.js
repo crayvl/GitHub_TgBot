ce7abee87c91455a5db6f5a4eabe8764c7abd823
crayvl <Kirillrusakov472@gmail.com>
2026-06-01 19:50:54 +0500

    Deploy for the test

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import express from 'express';
 const server = express();
 
 server.get('/ping', (req, res) => {
-  res.send('server active');
+  res.sendStatus(200);
 });
 
 server.post('/webhook', (req, res) => {
@@ -13,6 +13,6 @@ server.post('/webhook', (req, res) => {
 
 
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000\n http://localhost:3000');
+server.listen(3000,'0.0.0.0', () => {
+  console.log('Server is running.');
 });
