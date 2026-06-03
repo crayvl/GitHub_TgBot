2115eb7e00335d077ff386e6aa465ec4e816f2cd
crayvl <Kirillrusakov472@gmail.com>
2026-06-03 19:04:41 +0500

    Add more information in message

--- src/bot/telegram.js
+++ src/bot/telegram.js
@@ -11,11 +11,11 @@ let bot = null;
 
 export async function startBot() {
     if (bot) {
-        console.log('[Warning] Bot is already running');
+        console.log('[Warning] Bot is already running.');
         return bot;
     }
     
-    console.log('launching Telegram bot...');
+    console.log('launching Telegram bot.');
     bot = new TelegramBot(token, { polling: true });
 
     bot.onText(/\/start/, async (msg) => {
@@ -28,15 +28,40 @@ export async function startBot() {
 
 
 export async function sendWebhook(payload) {
-
     if (!bot) {
         console.error("[ERROR] Bot is not initialized.");
         return;
     }
-    let message = `📥 Новый пуш в ${payload.repository.full_name}\n👤 Пушер: ${payload.pusher.name}\n`;
+    const commits = payload.commits;
+
+    let message = `📥 Новый пуш в ${payload.repository.full_name}\n \
+                    👤 Пушер: ${payload.pusher.name}\n`;
+    
+    for(let i = 0; i < commits.length; i++)
+    {
+        const msg = commits[i].message.split("\n");
+        console.log(msg);
+
+        message += `\n~~~~~~~~${commits[i].timestamp.slice(10)}~~~~~~~~~~\n \
+            ~~~~~~~~~~~~${commits[i].timestamp.slice(11, 16)}~~~~~~~~~~~~\n \
+            📌 [commit #${i+1}] \
+            👤 Автор: ${commits[i].author.username}\n\n \
+            📝 Название:\n<b>${msg[0]}</b>\n`
+
+        if(msg.length >= 2) {
+        message += `📃 Описание:\n<blockquote>`
+        for(let y = 1; y < msg.length; y++) {
+            message += `${msg[y]}`
+        }
+        message += '</blockquote>'
+        }
+    }
+
+    
+    console.log(message);
 
     try {
-        await bot.sendMessage(chatID, message);
+        await bot.sendMessage(chatID, message, {parseMode: "HTML"});
         console.log("[INFO] The message has been delivered.");
     } catch(error){
         console.log("[ERROR] The message was not delivered.", error.message);
